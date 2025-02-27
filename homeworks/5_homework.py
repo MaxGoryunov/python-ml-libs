038931d734ff55428b37498c4b90291a130b97bb
Max Goryunov <maximthehedgehog@yandex.ru>
2025-02-27 12:39:36 +0300

    task 3

--- homeworks/5_homework.py
+++ homeworks/5_homework.py
@@ -9,31 +9,42 @@ import seaborn as sns
 
 print('Задание 1')
 
-x = np.array([1, 5, 10, 15, 20])
-y1 = np.array([1, 7, 3, 5, 11])
-y2 = np.array([4, 3, 1, 8, 12])
+# x = np.array([1, 5, 10, 15, 20])
+# y1 = np.array([1, 7, 3, 5, 11])
+# y2 = np.array([4, 3, 1, 8, 12])
 
-plt.plot(x, y1, '-ro', label='line 1')
-plt.plot(x, y2, '-.go', label='line 1')
-plt.legend()
+# plt.plot(x, y1, '-ro', label='line 1')
+# plt.plot(x, y2, '-.go', label='line 1')
+# plt.legend()
 
-plt.show()
+# plt.show()
 
 
 print('Задание 2')
 
-grid = plt.GridSpec(2, 2)
-ax = plt.subplot(grid[0, :])
-x = np.arange(1, 6)
-y = np.array([1, 7, 6, 3, 5])
-ax.plot(x, y)
+# grid = plt.GridSpec(2, 2)
+# ax = plt.subplot(grid[0, :])
+# x = np.arange(1, 6)
+# y = np.array([1, 7, 6, 3, 5])
+# ax.plot(x, y)
 
-ax = plt.subplot(grid[1, 0])
-y = np.array([9, 4, 2, 4, 9])
-ax.plot(x, y)
+# ax = plt.subplot(grid[1, 0])
+# y = np.array([9, 4, 2, 4, 9])
+# ax.plot(x, y)
 
-ax = plt.subplot(grid[1, 1])
-y = np.array([-7, -4, 2, -4, -7])
-ax.plot(x, y)
+# ax = plt.subplot(grid[1, 1])
+# y = np.array([-7, -4, 2, -4, -7])
+# ax.plot(x, y)
+
+# plt.show()
+
+
+print('Задание 3')
+
+x = np.arange(-5, 6)
+y = x ** 2
+plt.plot(x, y)
+plt.annotate('min', xy=(0, 0), xytext=(0, 10),
+             arrowprops=dict(facecolor='green'))
 
 plt.show()
